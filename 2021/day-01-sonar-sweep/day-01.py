cc8ea735b02be0d6d6deafe595b2bb987ddf1a6d
Priyal Rajgor <priyalrajgor@gmail.com>
2021-12-02 21:20:57 +1100

    Solution for Day 1

---
+++ 2021/day-01-sonar-sweep/day-01.py
@@ -0,0 +1,40 @@
+# =================================================================
+# IMPORT REQUIRED LIBRARIES
+# =================================================================
+import os
+
+# =================================================================
+# READ DATA
+# =================================================================
+
+data_location = os.path.join(os.path.abspath(""), '2021/day-01-sonar-sweep/')
+
+# with open(os.path.join(data_location, 'input_small.txt'), 'r') as f:
+with open(os.path.join(data_location, 'input.txt'), 'r') as f:
+    data = f.read().splitlines()
+
+data = list(map(int, data))
+
+# print(data)
+
+# =================================================================
+# LOGIC - PART ONE
+# =================================================================
+
+numSweeps = len(data)
+numChanges = 0
+for i in range(1 ,numSweeps):
+    if data[i] > data[i-1] : numChanges += 1
+
+# print(numChanges)
+
+# =================================================================
+# LOGIC - PART ONE
+# =================================================================
+
+numSweeps = len(data)
+numChanges = 0
+for i in range(3 ,numSweeps):
+    if sum(data[i-2:i+1]) > sum(data[i-3:i]) : numChanges += 1
+
+# print(numChanges)
